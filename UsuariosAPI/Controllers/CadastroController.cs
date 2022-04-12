5438aba19babb3a0865fa1f73659cfad1ab71526
Marcio Laranjeira dos Santos Vasconcelos <maxrjj@hotmail.com>
2022-04-12 09:12:23 -0300

    Aula_02_04 - Recebendo um usuário

---
+++ UsuariosAPI/Controllers/CadastroController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using UsuariosAPI.Data.DTO;
+
+namespace UsuariosAPI.Controllers
+{
+    [Microsoft.AspNetCore.Components.Route("[controller]")]
+    [ApiController]
+    public class CadastroController : ControllerBase    
+    {
+        [HttpPost]    
+        public IActionResult CadastrarUsuario(CreateUsuarioDTO usuario)
+        {
+            return Ok();
+        }
+
+    }
+}
---
+++ UsuariosAPI/Data/DTO/CreateUsuarioDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UsuariosAPI.Data.DTO
+{
+    public class CreateUsuarioDTO
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [Compare("Password", ErrorMessage = "Senha nao bate")]
+        public string RePassword { get; set; }
+    }
+}
